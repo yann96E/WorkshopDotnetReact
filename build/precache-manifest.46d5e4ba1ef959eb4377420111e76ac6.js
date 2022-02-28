4881fc6afb78b494cc8b8fafdc57a942a57d874b
yann96E <yannsimonyann96@hotmail.fr>
2022-02-28 10:34:24 +0100

    push

---
+++ build/precache-manifest.46d5e4ba1ef959eb4377420111e76ac6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7388fe80de5cc8646d992f46ffcd66d3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e2d53441ced2f4682385",
+    "url": "/static/css/main.9149988f.chunk.css"
+  },
+  {
+    "revision": "45ec1c0ef2bee29aaf39",
+    "url": "/static/js/2.be548e55.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.be548e55.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e2d53441ced2f4682385",
+    "url": "/static/js/main.7c300fbc.chunk.js"
+  },
+  {
+    "revision": "749f6ffc540e490ba3a2",
+    "url": "/static/js/runtime-main.271f8bd8.js"
+  }
+]);
